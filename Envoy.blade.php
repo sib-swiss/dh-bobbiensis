e3a928ed1d0ed4786cafe96b6440b0d3cd780a73
Silvano Aldà <silvano.alda@sib.swiss>
2023-08-28 12:19:28 +0200

    envoy

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,10 +1,10 @@
 @setup
     $repository = 'git@gitlab.sib.swiss:mark16-vre-group/bobbiensis.git';
 
-    // $server_dev = 'mark16@mark16-dev8.vital-it.ch';
+    $server_dev = 'webdev@bobbiensis-dev.vital-it.ch';
     // $server_prod = 'mark16@mark16-prod8.vital-it.ch';
 
-    // $deploy_path_dev = '/var/vhosts/vital-it.ch/mark16-dev/htdocs';
+    $deploy_path_dev = '/var/vhosts/vital-it.ch/bobbiensis-dev/htdocs';
     // $deploy_path_prod = '/var/vhosts/vital-it.ch/mark16-prod/htdocs';
 
     $app_dir = $server === 'prod' ? $deploy_path_prod : $deploy_path_dev ;
