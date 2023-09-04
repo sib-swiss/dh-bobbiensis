132d746f86308d27ee2e8eeeb373ae635973e5c2
Silvano Aldà <silvano.alda@sib.swiss>
2023-09-04 10:31:55 +0200

    vl 1 page

---
+++ resources/views/vl1.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="w-1/2 mx-auto">
+        <h1>VL 1</h1>
+        
+        <p>
+            Text for VL 1
+        </p>
+    </div>
+@endsection
